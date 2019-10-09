d8860754e9aff16c824b8332771e1f818ebda4d3
okeyobia <okey.obia@gmail.com>
2019-10-09 10:54:48 -0400

    added two users

--- src/main/java/com/okey/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/okey/cardatabase/CardatabaseApplication.java
@@ -1,9 +1,14 @@
 package com.okey.cardatabase;
 
+import com.okey.cardatabase.domain.User;
+import com.okey.cardatabase.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CardatabaseApplication {
@@ -13,5 +18,21 @@ public class CardatabaseApplication {
 		SpringApplication.run(CardatabaseApplication.class, args);
 		logger.info("Hello Spring Boot!!");
 	}
+
+    @Autowired private UserRepository userRepository;
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            // username: user password: user
+            userRepository.save(new User("user",
+                    "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi",
+                    "USER"));
+            // username: admin password: admin
+            userRepository.save(new User("admin",
+                    "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG",
+                    "ADMIN"));
+        };
+    }
 
 }
